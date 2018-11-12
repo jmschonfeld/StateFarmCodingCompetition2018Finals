f774f1e20d6bbe234cf3c371fad07c0c2f126435
jmschonfeld <jeremyschonfeld@gmail.com>
2018-11-12 10:51:03 -0600

    Copyright notice

--- _Project/src/main/java/me/jeremyrobert/sf2018/ui/MainContentPane.java
+++ _Project/src/main/java/me/jeremyrobert/sf2018/ui/MainContentPane.java
@@ -2,6 +2,7 @@ package me.jeremyrobert.sf2018.ui;
 
 import java.awt.BorderLayout;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 
@@ -16,6 +17,7 @@ public class MainContentPane extends JPanel {
 		tabs.addTab("Location Reports", new GenerateReportsPanel());
 		this.setLayout(new BorderLayout());
 		this.add(tabs, BorderLayout.CENTER);
+		this.add(new JLabel(" © OpenStreetMap contributors, some data retrieved from Data.gov (https://www.data.gov/)"), BorderLayout.SOUTH);
 	}
 
 }
